3fcbe2708068b45a4bb9a69f98e8c336ee9f6d73
ajha-digi <ajha@digisprint.com>
2023-10-05 15:13:10 +0530

    implemented useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useRef, useEffect } from "react";
+import "./style.css"
 function App() {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    // inputRef.current.focus();
+    inputRef.current.disabled = true;
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+    inputRef.current.disabled = false;
+  };
+
   return (
-   <h1>Hello World !!</h1>
-  )
+    <>
+      <h1>Hello World !!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          ref={inputRef}
+        />
+        <button type="submit">Display</button>
+      </form>
+    </>
+  );
 }
 
-export default App
+export default App;
